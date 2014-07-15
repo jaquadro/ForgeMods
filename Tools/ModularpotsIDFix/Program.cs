9e8b04baa90ad5d087c16ac877b8cf3ca25d1473
Justin Aquadro <jaquadro@gmail.com>
2014-07-14 21:04:49 -0400

    Thought I published this tool, but I didn't?

---
+++ Tools/ModularpotsIDFix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Substrate.Core;
+using Substrate.Nbt;
+
+namespace ModularpotsIDFix
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            if (!File.Exists("level.dat")) {
+                Console.WriteLine("Could not find level.dat");
+                return;
+            }
+
+            NBTFile nf = new NBTFile("level.dat");
+            NbtTree tree;
+
+            using (Stream nbtstr = nf.GetDataInputStream()) {
+                if (nbtstr == null) {
+                    Console.WriteLine("Could not open level.dat");
+                    return;
+                }
+
+                tree = new NbtTree(nbtstr);
+            }
+
+            TagNodeList list = tree.Root["FML"].ToTagCompound()["ItemData"].ToTagList();
+            foreach (TagNodeCompound tag in list) {
+                TagNodeString modid = tag["K"].ToTagString();
+                if (modid.Data.Contains("modularpots:modularpots:")) {
+                    modid.Data = modid.Data.Replace("modularpots:modularpots:", "modularpots:");
+                    Console.WriteLine("Updating entry " + tag["V"].ToTagInt().Data + ": " + modid.Data);
+                }
+            }
+
+            using (Stream zipstr = nf.GetDataOutputStream()) {
+                if (zipstr == null) {
+                    Console.WriteLine("Could not write back to level.dat");
+                    return;
+                }
+
+                tree.WriteTo(zipstr);
+            }
+
+            Console.WriteLine("Update complete");
+        }
+    }
+}
